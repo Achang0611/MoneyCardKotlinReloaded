9918d5d5a2e27e03828d5b3fa76a06b02b4bab2f
vmtayvj <vmtayvj@gmail.com>
2021-03-15 19:41:19 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "com.vm"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven { url = URI.create("https://hub.spigotmc.org/nexus/content/repositories/snapshots") }
+    maven { url = URI.create("https://oss.sonatype.org/content/repositories/snapshots/") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    compileOnly("org.spigotmc:spigot-api:1.15.2-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<ProcessResources> {
+    filesMatching("plugin.yml") {
+        expand(project.properties)
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    archiveVersion.set("")
+}
---
+++ src/main/kotlin/com/vm/plugin/MinecraftKotlinPlugin.kt
@@ -0,0 +1,12 @@
+package com.vm.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MinecraftKotlinPlugin : JavaPlugin() {
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+
+    }
+}
